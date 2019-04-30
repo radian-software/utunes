96b1742b91740dc675e9ac9d90b8cd75539cd7fa
Radon Rosborough <radon.neon@gmail.com>
2019-04-30 09:03:34 -0700

    Initialize Emacs Lisp project

---
+++ utunes.el
@@ -0,0 +1,41 @@
+;;; utunes.el --- Microscopic music library manager. -*- lexical-binding: t -*-
+
+;; Copyright (C) 2019 Radon Rosborough
+
+;; Author: Radon Rosborough <radon.neon@gmail.com>
+;; Created: 30 Apr 2019
+;; Homepage: https://github.com/raxod502/utunes
+;; Keywords: applications
+;; Package-Requires: ((emacs "26"))
+;; Version: 0
+
+;;; Commentary:
+
+;; µTunes attempts to replace major functionality of a full-featured
+;; music library manager such as iTunes with an absolute minimum
+;; number of lines of code. The interface, as such, is designed to
+;; maximize flexibility while minimizing implementation complexity and
+;; number of abstractions.
+
+;; Please see <https://github.com/raxod502/utunes> for more
+;; information.
+
+;;; Code:
+
+;; To see the outline of this file, run M-x outline-minor-mode and
+;; then press C-c @ C-t. To also show the top-level functions and
+;; variable declarations in each section, run M-x occur with the
+;; following query: ^;;;;* \|^(
+
+
+
+;;;; Closing remarks
+
+(provide 'utunes)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; outline-regexp: ";;;;* "
+;; End:
+
+;;; el-patch.el ends here
