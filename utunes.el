311f0cb95c6cc9db4f5195b3038e6a9522192649
Radon Rosborough <radon.neon@gmail.com>
2019-05-22 15:00:45 -0600

    Add preliminary functions to utunes.el

--- utunes.el
+++ utunes.el
@@ -27,7 +27,93 @@
 ;; variable declarations in each section, run M-x occur with the
 ;; following query: ^;;;;* \|^(
 
+(require 'cl-lib)
+(require 'subr-x)
 
+(defgroup utunes nil
+  "Emacs frontend for the microscopic music library manager."
+  :group 'applications
+  :prefix "utunes-")
+
+(defcustom utunes-log-buffer-name "*utunes-log*"
+  "Name of the buffer used for diagnostic output from µTunes commands."
+  :type 'string)
+
+(defun utunes--get-log-buffer ()
+  "Return the buffer for diagnostic output from µTunes commands.
+If the buffer does not exist, create it first."
+  (if-let ((buf (get-buffer utunes-log-buffer-name)))
+      buf
+    (with-current-buffer (get-buffer-create utunes-log-buffer-name)
+      (special-mode)
+      (current-buffer))))
+
+(defun utunes--capitalize (str)
+  "Capitalize only the first character of STR."
+  (if (string-empty-p str)
+      str
+    (concat (upcase (substring str 0 1)) (substring str 1))))
+
+(defun utunes--process-sentinel (proc event)
+  "Process sentinel for µTunes backend commands.
+PROC is the process and EVENT is a string describing the event
+that happened. See
+<https://www.gnu.org/software/emacs/manual/html_node/elisp/Sentinels.html>."
+  (unless (process-live-p proc)
+    (with-current-buffer (utunes--get-log-buffer)
+      (let ((inhibit-read-only t))
+        (save-excursion
+          (goto-char (point-max))
+          (insert "[" (utunes--capitalize (string-trim event)) "]\n")))
+      (unless (= 0 (process-exit-status proc))
+        (message "µTunes backend command failed")
+        (pop-to-buffer (current-buffer))
+        (goto-char (point-max))))
+    (setq utunes--current-process nil)))
+
+(defvar utunes--last-process nil
+  "µTunes process currently or last running.
+Used to prevent running more than one µTunes command
+concurrently.")
+
+(cl-defun utunes-run-backend-command (&key args stdin stdout)
+  "Run a µTunes backend command with given command-line ARGS.
+ARGS should not include \"utunes\".
+
+If provided, STDIN should be a buffer whose contents will be fed
+to the command on stdin. Similarly, if provided, STDOUT should be
+a buffer to whose contents the command will append from stdout.
+
+If a µTunes command is already running, return an error."
+  (cl-block nil
+    (when (process-live-p utunes--last-process)
+      (error "µTunes process already running"))
+    (with-current-buffer (utunes--get-log-buffer)
+      (let ((args (cons "utunes" args))
+            (inhibit-read-only t))
+        (save-excursion
+          (goto-char (point-max))
+          (unless (= (point) (point-min))
+            (insert "\n"))
+          (insert "$ " (mapconcat #'shell-quote-argument args " ") "\n"))
+        (condition-case _err
+            (setq utunes--last-process
+                  (make-process
+                   :name "utunes"
+                   :buffer stdout
+                   :command args
+                   :stderr (current-buffer)
+                   :sentinel #'utunes--process-sentinel))
+          (file-missing
+           (insert "[Command not found]\n")
+           (cl-return)))
+        (set-process-sentinel (get-buffer-process (current-buffer))
+                              #'ignore)
+        (when stdin
+          (with-current-buffer stdin
+            (process-send-string
+             utunes--last-process
+             (point-min) (point-max))))))))
 
 ;;;; Closing remarks
 
